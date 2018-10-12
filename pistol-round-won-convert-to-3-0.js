00c4bcee8293d9c44a3ea442d5f3a6dc36c2c0e4
William Martin Stewart <zoidbergwill@gmail.com>
2018-10-12 08:41:35 +0200

    Add a script for starting a half 3-0 (#2)
    * Add a script for starting a half 3-0
    
    https://docs.google.com/spreadsheets/d/10CsgtOzhlAfKrhJjImTT8YHonLxRzs7b69MT3yoGI6Q/edit?usp=sharing
    
    * Update pistol-round-won-convert-to-3-0.js

---
+++ pistol-round-won-convert-to-3-0.js
@@ -0,0 +1,60 @@
+/* Calculate whether winning pistol round was converted to a 3-0 score difference
+ */
+
+if (process.argv.length != 3) {
+  console.log('Please specify the full path to a JSON file');
+  console.log('e.g.: ./rounds.js ~/input.json');
+
+  process.exit(1);
+}
+
+var fs = require('fs');
+var path = require('path');
+var d = require('./' + process.argv[2]);
+
+if (d.parser_name !== 'ghostanalysis.dem2json-events' || d.parser_version !== '0.0.x-dev') {
+  console.log('Bad JSON version!');
+  process.exit(1);
+}
+
+let roundNum;
+let overtime = false;
+let postPistolRoundCount = 0;
+let times = 0;
+let winners = {};
+
+d.events.forEach(function (e) {
+  if (e.type === 'round_start' || e.type === 'round_freeze_end') {
+    if (e.round !== roundNum) {
+      roundNum = e.round;
+      if (roundNum === 0) {
+        times = 0;
+      }
+      if (e.round > 30) {
+        overtime = true
+      }
+      postPistolRoundCount = (roundNum - 1) % 15
+      if (postPistolRoundCount == 0) {
+        winners = {};
+      }
+    }
+  }
+  if (e.type === 'round_end') {
+    if (!overtime && postPistolRoundCount < 3) {
+      // console.log(e.winner);
+      if (winners[e.winner] == undefined) {
+        winners[e.winner] = 0;
+      }
+      winners[e.winner] += 1;
+      if (postPistolRoundCount == 2) {
+        for (const [team, wins] of Object.entries(winners)) {
+          if (wins == 3) {
+            times++;
+          }
+        }
+      }
+    }
+  }
+});
+
+console.log(`${path.basename(process.argv[2], '.json')}, ${times}`);
